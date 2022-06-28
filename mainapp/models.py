fe0d5b14a5c0ac63314ad843578ff99d5cbf9d31
Boburjon Dadajonov <dadajonovboburjon3@gmail.com>
2022-06-28 08:27:09 +0500

    template

--- mainapp/models.py
+++ mainapp/models.py
@@ -5,14 +5,31 @@ class FutbolKlub(models.Model):
     name = models.CharField(max_length=255)
     img = models.ImageField(upload_to='klub')
 
-
+    def __str__(self):
+        return self.name
 
 class Turnir(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)    
     
-    
+    def __str__(self):
+        return self.name
     
 class TurnirTur(models.Model):
+    name = models.CharField(max_length=255)
     turnir = models.ForeignKey(Turnir, on_delete=models.CASCADE)
-    klub_bir = models.ForeignKey(FutbolKlub, on_delete=models.CASCADE)
-    klub_ikki = models.ForeignKey(FutbolKlub, on_delete=models.CASCADE)
+    data_start = models.DateField()
+
+    def __str__(self):
+        return self.name
+
+class Match(models.Model):
+    data = models.DateField()
+    turnir_tur = models.ForeignKey(TurnirTur, on_delete=models.CASCADE)
+    first_klub = models.ForeignKey(FutbolKlub, on_delete=models.CASCADE, related_name='bir')
+    first_klub_result = models.PositiveIntegerField(blank=True, null=True)
+    second_klub = models.ForeignKey(FutbolKlub, on_delete=models.CASCADE, related_name='ikki')
+    second_klub_result = models.PositiveIntegerField(blank=True, null=True)
+    winner = models.ForeignKey(FutbolKlub, on_delete=models.CASCADE, blank=True, null=True, related_name='golib')
+
+    def __str__(self):
+        return self.first_klub, self.second_klub
